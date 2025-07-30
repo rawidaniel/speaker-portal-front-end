d005bff89158413eee0f2fcc35d600ca9a998920
rawidaniel <rawidaniel@gmail.com>
2025-07-30 18:40:09 +0300

    feat: add signup page

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Login from "./components/Login";
+import Signup from "./components/SIgnup";
 
 const App = () => (
   <BrowserRouter>
     <Routes>
       <Route path="/login" element={<Login />} />
+      <Route path="/signup" element={<Signup />} />
     </Routes>
   </BrowserRouter>
 );
